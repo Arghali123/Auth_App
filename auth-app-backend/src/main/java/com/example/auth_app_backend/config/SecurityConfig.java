0bb8c7d3797505655d866260cdfc81967cee3da4
Denish Arghali <150317382+Arghali123@users.noreply.github.com>
2026-04-07 14:08:59 +0545

    Managing users completed

---
+++ auth-app-backend/src/main/java/com/example/auth_app_backend/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.auth_app_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeHttpRequests->
+                authorizeHttpRequests.requestMatchers("/api/v1/auth/register").permitAll()
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .requestMatchers("/api/v1/users").permitAll()
+                        .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService users()
+//    {
+//        User.UserBuilder userBuilder=User.withDefaultPasswordEncoder();
+//        UserDetails user1=userBuilder.username("daenish").password("abc").roles("ADMIN").build();
+//        UserDetails user2=userBuilder.username("niraj").password("xyz").roles("ADMIN").build();
+//        UserDetails user3=userBuilder.username("dinesh").password("123").roles("USER").build();
+//        return new InMemoryUserDetailsManager(user1,user2,user3);
+//    }
+}
--- auth-app-backend/src/main/java/com/example/auth_app_backend/services/impl/AuthServiceImpl.java
+++ auth-app-backend/src/main/java/com/example/auth_app_backend/services/impl/AuthServiceImpl.java
@@ -4,20 +4,23 @@ import com.example.auth_app_backend.dtos.UserDTO;
 import com.example.auth_app_backend.services.AuthService;
 import com.example.auth_app_backend.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDTO registerUser(UserDTO userDTO) {
         //logic
-        //verify
+        //verify email
         //verify password
         //default roles
 
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         UserDTO userDTO1=userService.createUser(userDTO);
         return userDTO1;
     }
