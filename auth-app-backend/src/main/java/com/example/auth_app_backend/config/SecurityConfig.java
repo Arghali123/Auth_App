53d47f4d20942bce25b3f08d1dc87b27590518a3
Denish Arghali <150317382+Arghali123@users.noreply.github.com>
2026-04-15 15:08:25 +0545

    Oauth with Google completed

--- auth-app-backend/src/main/java/com/example/auth_app_backend/config/SecurityConfig.java
+++ auth-app-backend/src/main/java/com/example/auth_app_backend/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import tools.jackson.databind.ObjectMapper;
 
@@ -22,10 +23,16 @@ import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationSuccessHandler successHandler;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,AuthenticationSuccessHandler successHandler)
+    {
+        this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+        this.successHandler=successHandler;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -40,6 +47,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/logout").permitAll()
                         .anyRequest().authenticated()
         )
+                .oauth2Login(oauth2->oauth2.successHandler(successHandler).failureHandler(null)).logout(AbstractHttpConfigurer::disable)
                 .exceptionHandling(ex->ex.authenticationEntryPoint((request, response, e) -> {
                     //error message
                     //e.printStackTrace();
